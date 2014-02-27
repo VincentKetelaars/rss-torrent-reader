f57e8136059a555c1773d5ee428c699be5dc9979
Vincent Ketelaars <vincentketelaars@gmail.com>
2014-02-27 16:04:34 +0100

    Created custom logger that ensures the directory of the log file location exists, and if not creates

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,16 @@ Created on Dec 24, 2013
 @author: Vincent Ketelaars
 '''
 import logging
+import os
 
 def get_logger(name):
     logger = logging.getLogger(name)
     return logger
+
+class SafeTimedRotatingFileHandler(logging.handlers.TimedRotatingFileHandler):
+    
+    def __init__(self, filename, when='h', interval=1, backupCount=0, encoding=None, delay=False, utc=False):
+        dirs = os.path.dirname(filename)
+        if not os.path.exists(dirs):
+            os.makedirs(dirs)
+        logging.handlers.TimedRotatingFileHandler.__init__(self, filename, when=when, interval=interval, backupCount=backupCount, encoding=encoding, delay=delay, utc=utc)
